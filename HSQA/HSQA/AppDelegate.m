6bb0abdad4343be1553ee63ad0506e593c38ca18
Chenchen Zheng <chenchen.zheng@czheng2-mbp.fireeye.com>
2015-10-07 12:11:30 -0700

    copy code from default example

--- HSQA/HSQA/AppDelegate.m
+++ HSQA/HSQA/AppDelegate.m
@@ -12,34 +12,66 @@
 
 @end
 
-@implementation AppDelegate
+@implementation AppDelegate {
+    UIApplicationShortcutItem *launchedShortcutItem;
+}
 
+- (BOOL) handleShortCutItem:(UIApplicationShortcutItem *)shortcutItem {
+    BOOL handled = false;
+    
+    NSString *shortCutType = shortcutItem.type;
+    if (shortCutType != nil && shortCutType.length != 0) {
+        if ([shortCutType isEqualToString:@"com.iamchenchen.HSQA.openfavorites"]) {
+            handled = true;
+        } else if ([shortCutType isEqualToString:@"com.iamchenchen.HSQA.newmessage"]) {
+            handled = true;
+        }
+    } else {
+        return false;
+    }
+    
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"ShortCut Handled" message:shortcutItem.localizedTitle preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil];
+    [alert addAction:okAction];
+    
+    [self.window.rootViewController presentViewController:alert animated:YES completion:nil];
+    
+    return handled;
+}
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
-    return YES;
-}
-
-- (void)applicationWillResignActive:(UIApplication *)application {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-}
-
-- (void)applicationDidEnterBackground:(UIApplication *)application {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-}
-
-- (void)applicationWillEnterForeground:(UIApplication *)application {
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    
+    BOOL shouldPerformAdditionalDelegateHandling = true;
+    UIApplicationShortcutItem *shortcutItem = [launchOptions objectForKey:UIApplicationLaunchOptionsShortcutItemKey];
+    
+    if (shortcutItem != nil) {
+        //use samething!
+        
+        launchedShortcutItem = shortcutItem;
+        
+        shouldPerformAdditionalDelegateHandling = false;
+        
+    }
+    
+    return shouldPerformAdditionalDelegateHandling;
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    if (launchedShortcutItem == nil) {
+        return;
+    }
+    
+    [self handleShortCutItem:launchedShortcutItem];
+    
+    launchedShortcutItem = nil;
 }
 
-- (void)applicationWillTerminate:(UIApplication *)application {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+- (void)application:(UIApplication *)application performActionForShortcutItem:(UIApplicationShortcutItem *)shortcutItem completionHandler:(void (^)(BOOL))completionHandler {
+    
+    BOOL handleShortCutItem = [self handleShortCutItem:shortcutItem];
+    
+    completionHandler(handleShortCutItem);
+    
 }
 
 @end
