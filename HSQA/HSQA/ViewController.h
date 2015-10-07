5c3ce6e837b0ed9daf34a3739f32132bcfd99c4c
Chenchen Zheng <chenchen.zheng@czheng2-mbp.fireeye.com>
2015-10-07 12:06:37 -0700

    add Home Screen Quick Action xcode project: HSQA

---
+++ HSQA/HSQA/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HSQA
+//
+//  Created by Chenchen Zheng on 10/7/15.
+//  Copyright © 2015 Chenchen Zheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HSQA/HSQA/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  HSQA
+//
+//  Created by Chenchen Zheng on 10/7/15.
+//  Copyright © 2015 Chenchen Zheng. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
